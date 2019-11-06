a307af104d6a3e15fef29333c510c4f805a742fc
Ardhio Wibowo <ardhio.sutjipto@mitrais.com>
2019-11-06 12:22:13 +0800

    Try fixing the issue on iPhone

--- iphone-input-focus/script.js
+++ iphone-input-focus/script.js
@@ -6,7 +6,34 @@ $(document).ready(function() {
 		} else {
 			$('.wr-dropdown').hide();
 			$('.mobile-search').show();
+			// $('.mobile-search').find('input').focus();
+			
+			// create invisible dummy input to receive the focus first
+			const fakeInput = document.createElement('input')
+			fakeInput.setAttribute('type', 'text')
+			fakeInput.style.position = 'absolute'
+			fakeInput.style.opacity = 0
+			fakeInput.style.height = 0
+			fakeInput.style.fontSize = '16px' // disable auto zoom
+
+			// you may need to append to another element depending on the browser's auto 
+			// zoom/scroll behavior
+			document.body.prepend(fakeInput)
+
+			// focus so that subsequent async focus will work
+			fakeInput.focus()
+
+			setTimeout(() => {
+
+			// now we can focus on the target input
+			// targetInput.focus()
 			$('.mobile-search').find('input').focus();
+
+			// cleanup
+			fakeInput.remove()
+
+			}, 1000);
+
 		}
 	});
 	
